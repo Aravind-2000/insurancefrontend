0d7a386cef94077cce1de326798e6b1be6cac3c4
Sanara-1930 <lakshmi.nv@futurainstech.com>
2022-03-05 07:03:14 +0530

    No changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App.js';
+
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
 
