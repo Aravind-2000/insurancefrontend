a1f6a50c316025d312d2b6394f233cb80d803ca0
Sanara-1930 <lakshmi.nv@futurainstech.com>
2022-03-05 07:02:02 +0530

    adding routes for address, bank and client personal

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ import Mainpage from "./Components/Mainpage";
 import ListEmployee from "./Components/Employee/ListEmployee";
 import EmployeeDashboard from "./Components/Employee/EmployeeDashboard";
 import Navbar from "./Components/Header/Navbar";
+import NotificationTable from "./Components/Notification/NotificationTable";
+import BankAccount from "./Components/BankAccount/BankAccount";
+import ClientAddress from "./Components/Address/ClientAddress";
+import ClientDetails from "./Components/Client/ClientDetails";
+import ClientAddressAdd from "./Components/Address/ClientAddressAdd";
 
 const App = () => {
   return (
       <div>
           <Navbar/>
-          <div className="container container-md container-lg">
+          <div>
               <br/> <br/>
               <Routes>
                   <Route exact path={"/"} element={<Mainpage/>} />
@@ -20,6 +25,11 @@ const App = () => {
                   <Route  exact path={"/add"} element={<AddCandidate/>}/>
                   <Route  exact path={"/employee"} element={<ListEmployee/>}/>
                   <Route exact path={"/emp-dashboard"} element={<EmployeeDashboard/>} />
+                  <Route exact path={"/notification"} element={<NotificationTable/>} />
+                  <Route exact path={"/bank"} element={<BankAccount/>} />
+                  <Route exact path={"/address"} element={<ClientAddress/>} />
+                  <Route exact path={"/address-add"} element={<ClientAddressAdd/>} />
+                  <Route exact path={"/client"} element={<ClientDetails/>} />
               </Routes>
           </div>
       </div>
