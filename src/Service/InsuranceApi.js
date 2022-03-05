bf544a8c8e0090137d651d3f517277e8f92daf0a
Sanara-1930 <lakshmi.nv@futurainstech.com>
2022-03-05 07:04:39 +0530

    adding API methods for address, notification and client personal

--- src/Service/InsuranceApi.js
+++ src/Service/InsuranceApi.js
@@ -12,6 +12,14 @@ const quantsbyId = "http://localhost:8090/quants/"
 const updateQuants = "http://localhost:8090/quants/save";
 const nameLike = "http://localhost:8090/candidates/search/name/";
 const search = "http://localhost:8090/candidates/search/";
+const notification = "http://localhost:8090/notification/employee/";
+const getAllAddress = "http://localhost:8090/address/getall";
+const addAddress = "http://localhost:8090/address/add";
+const updateaddress = "http://localhost:8090/address/";
+const allClients = "http://localhost:8090/client/getall";
+const getclient = "http://localhost:8090/client/";
+const saveClient = "http://localhost:8090/client/add";
+const updateclient = "http://localhost:8090/client/"
 
 
 class InsuranceApi{
@@ -52,6 +60,33 @@ class InsuranceApi{
     seacrhAll(value){
          return axios.get(search + value);
     }
+    getEmployeeNotification(id){
+         return axios.get(notification + id);
+    }
+    getAllAddress(){
+         return axios.get(getAllAddress);
+    }
+    saveAddress(address){
+         return axios.post(addAddress , address);
+    }
+    updateAddress(id, value){
+         return axios.patch(updateaddress + id , value);
+    }
+    getAllClients(){
+         return axios.get(allClients);
+    }
+    getClient(id){
+         return axios.get(getclient + id);
+    }
+    addClients(client){
+         return axios.post(saveClient, client);
+    }
+    updateClient(id, client){
+         return axios.patch(updateclient + id, client)
+    }
+
+
+
 }
 
 export default new InsuranceApi();
