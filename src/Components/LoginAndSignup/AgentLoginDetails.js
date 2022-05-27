54109b02ae38d01480a3baedc2abf0a953350931
Sanara-1930 <lakshmi.nv@futurainstech.com>
2022-05-27 16:49:52 +0530

    added a carousel like structure for viewing my trainings

--- src/Components/LoginAndSignup/AgentLoginDetails.js
+++ src/Components/LoginAndSignup/AgentLoginDetails.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import {Paper} from "@material-ui/core";
-import {Row, Col, Modal} from "react-bootstrap";
+import { Modal, Carousel} from "react-bootstrap";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
 import LogoutIcon from '@mui/icons-material/Logout';
@@ -20,17 +20,21 @@ const AgentLoginDetails = () => {
 
     const paperStyle = {
         padding: 20,
-        height: "120vh",
-        width: 1000,
-        margin: "10px 400px auto",
+        height: "125vh",
+        marginLeft:10,
+        marginTop:10,
+        width:"max-width"
+
     };
 
     const paperStyle1 = {
         padding: 20,
         height: "55vh",
-        width: 1000,
-        margin: "20px 0px 20px 400px"
+        width:600,
+        marginTop:20,
+        marginLeft: 175
     }
+
 
     const agentid = sessionStorage.getItem("agent");
     const userid = sessionStorage.getItem("userid");
@@ -122,130 +126,134 @@ const AgentLoginDetails = () => {
 
     return (
         <div>
-                {
-                    access?.find(element => element === "add-permission") ?
-                        <Button style={{color: "white", backgroundColor: "blue", marginLeft: 400}}
-                                onClick={() => showPermission()}>
-                            <SwitchAccessShortcutAddIcon/>
-                        </Button> : null
-                }
+                <div style={{display:"flex", marginLeft:10}}>
+                    <div>
+                        {
+                            access?.find(element => element === "add-permission") ?
+                                <Button style={{color: "white", backgroundColor: "blue"}}
+                                        onClick={() => showPermission()}>
+                                    <SwitchAccessShortcutAddIcon/>
+                                </Button> : null
+                        }
+                    </div>
 
-                {
-                    access?.find(element => element === "do-promote-demote") ?
-                        <Button style={{color: "white", backgroundColor: "blue", marginLeft: 100}}
-                                onClick={() => showPromote()}>
-                            <ImportExportIcon/>
-                        </Button> : null
-                }
-
-            <Paper elevation={5} style={paperStyle}>
-                <div style={{display:"flex",color: "white", marginLeft:850}}>
-                    <Button
-                        color="error"
-                        variant="contained"
-                        style={{paddingLeft:60, paddingRight: 55}}
-                        onClick={() => setModal(true)}>
-                    <h6> LOGOUT </h6>
-                    <LogoutIcon
-                        style={{  marginLeft:10, cursor:"pointer"}}
-                    /></Button>
+                    <div>
+                        {
+                            access?.find(element => element === "do-promote-demote") ?
+                                <Button style={{color: "white", backgroundColor: "blue"}}
+                                        onClick={() => showPromote()}>
+                                    <ImportExportIcon/>
+                                </Button> : null
+                        }
+                    </div>
                 </div>
-                <h3> Personal Details  </h3>
-                <br/>
-                <h4> User Details  <EditIcon style={{marginLeft:"10px", marginBottom:"2px",cursor:"pointer"}} onClick={() => userEditOpen()} />  </h4>
-                <h6> User ID :  {sessionStorage.getItem("userid")} </h6>
-                <h6> User Name :  {userDetails.username} </h6>
-                <h6> User E-Mail : {userDetails.email}  </h6>
-                <h5>  Role Details:  </h5>
-                <h6> Role Name : {userDetails?.role?.roleName} </h6>
-                <br/>
-                <h4> Agent Details </h4>
-                {
-                    sessionStorage.getItem("agent") === "null"  ?
-                        <h4> You are not a agent yet. </h4> :  agent === "UNAUTHORIZED" ? <h4> You are Unauthorized </h4> :
-                        <>
-                        <h6> Agent ID  : {agent.id} </h6>
-                        <h6> Agent Name: {agent.client?.givenName} {agent.client?.surName} </h6>
-                        <h6> Gender : {agent.client?.gender} </h6>
-                        <h6> Agent Personal Mail : {agent.client?.email} </h6>
-                        <h6> Agent Personal Number : {agent.client?.mobileNumber} </h6>
-                        <h6> Agent Occupation : {agent.client?.occupation} </h6>
-                        <br/>
-                        <h5> Agent Type Description </h5>
-                        <h6> Agent Description : {agent.agentTypeLevel?.agentLevelDesc} - {agent.agentTypeLevel?.agentLevelId} </h6>
-                        <br/>
-                        <h5>  Address Details </h5>
-                        <h6> Address : {agent.client?.address?.addressLine1} {agent.client?.address?.addressLine2}  {agent.client?.address?.city} ,  {agent.client?.address?.state} ,  {agent.client?.address?.country}. </h6>
-                        <h6> City : {agent.client?.address?.city} </h6>
-                        <h6>  State: {agent.client?.address?.state} </h6>
-                        <h6> Postal Code : {agent.client?.address?.pincode} </h6>
-                        <h6> Address Type  : {agent.client?.address?.addressType} </h6>
-                        <br/>
-                        <h5>  Bank Details </h5>
-                        <h6> Account Number : {agent?.client?.bankAccount?.accountNumber} </h6>
-                        <h6> Account Holder Name : {agent?.client?.bankAccount?.accountHolderName} </h6>
-                        <h6> IFSC Code : {agent?.client?.bankAccount?.ifscCode} </h6>
-                        <h6> Bank Name : {agent?.client?.bankAccount?.bankName} </h6>
-                        <h6> Bank Branch : {agent?.client?.bankAccount?.bankBranch} </h6>
-                        <br/>
-                        <h5> Commission Class </h5>
-                        <Row>
-                        <Col>
-                        <h6> Basic Commission : {agent.basicCommission} </h6>
-                        </Col>
-                        <Col>
-                        <h6> Service Commission : {agent.servicingCommission} </h6>
-                        </Col>
-                        <Col>
-                        <h6> Renewal Commission : {agent.renewalCommission} </h6>
-                        </Col>
-                        <Col>
-                        <h6>  Commission Class : {agent.commissionClass} </h6>
-                        </Col>
-                        </Row>
-                        </>
-                }
-            </Paper>
 
+                <div className="row">
+                    <div className="col">
+                        <Paper elevation={5} style={paperStyle}>
+                            <div style={{display:"flex",color: "white", marginLeft:700}}>
+                                <Button
+                                    color="error"
+                                    variant="contained"
+                                    onClick={() => setModal(true)}>
+                                    <h6> LOGOUT </h6>
+                                    <LogoutIcon
+                                        style={{  marginLeft:10, cursor:"pointer"}}
+                                    /></Button>
+                            </div>
+                            <h3> Personal Details  </h3>
+                            <br/>
+                            <h4> User Details  <EditIcon style={{marginLeft:"10px", marginBottom:"2px",cursor:"pointer"}} onClick={() => userEditOpen()} />  </h4>
+                            <h6> User ID :  {sessionStorage.getItem("userid")} </h6>
+                            <h6> User Name :  {userDetails.username} </h6>
+                            <h6> User E-Mail : {userDetails.email}  </h6>
+                            <h5>  Role Details:  </h5>
+                            <h6> Role Name : {userDetails?.role?.roleName} </h6>
+                            <br/>
+                            <h4> Agent Details </h4>
+                            {
+                                sessionStorage.getItem("agent") === "null"  ?
+                                    <h4> You are not a agent yet. </h4> :  agent === "UNAUTHORIZED" ? <h4> You are Unauthorized </h4> :
+                                        <>
+                                            <h6> Agent ID  : {agent.id} </h6>
+                                            <h6> Agent Name: {agent.client?.givenName} {agent.client?.surName} </h6>
+                                            <h6> Gender : {agent.client?.gender} </h6>
+                                            <h6> Agent Personal Mail : {agent.client?.email} </h6>
+                                            <h6> Agent Personal Number : {agent.client?.mobileNumber} </h6>
+                                            <h6> Agent Occupation : {agent.client?.occupation} </h6>
+                                            <br/>
+                                            <h5> Agent Type Description </h5>
+                                            <h6> Agent Description : {agent.agentTypeLevel?.agentLevelDesc} - {agent.agentTypeLevel?.agentLevelId} </h6>
+                                            <br/>
+                                            <h5>  Address Details </h5>
+                                            <h6> Address : {agent.client?.address?.addressLine1} {agent.client?.address?.addressLine2}  {agent.client?.address?.city} ,  {agent.client?.address?.state} ,  {agent.client?.address?.country}. </h6>
+                                            <h6> City : {agent.client?.address?.city} </h6>
+                                            <h6>  State: {agent.client?.address?.state} </h6>
+                                            <h6> Postal Code : {agent.client?.address?.pincode} </h6>
+                                            <h6> Address Type  : {agent.client?.address?.addressType} </h6>
+                                            <br/>
+                                            <h5>  Bank Details </h5>
+                                            <h6> Account Number : {agent?.client?.bankAccount?.accountNumber} </h6>
+                                            <h6> Account Holder Name : {agent?.client?.bankAccount?.accountHolderName} </h6>
+                                            <h6> IFSC Code : {agent?.client?.bankAccount?.ifscCode} </h6>
+                                            <h6> Bank Name : {agent?.client?.bankAccount?.bankName} </h6>
+                                            <h6> Bank Branch : {agent?.client?.bankAccount?.bankBranch} </h6>
+                                            <br/>
+                                            <h5> Commission Class </h5>
+                                            <h6> Basic Commission : {agent.basicCommission} </h6>
+                                            <h6> Service Commission : {agent.servicingCommission} </h6>
+                                            <h6> Renewal Commission : {agent.renewalCommission} </h6>
+                                            <h6> Commission Class : {agent.commissionClass} </h6>
+                                        </>
+                            }
+                        </Paper>
+                    </div>
+                    <div className="col">
+                        <div>
+                            <h3> My Trainings :- </h3>
+                            {
+                                sessionStorage.getItem("agent" ) === "null"  ?
+                                    <h4> You are not a agent yet. </h4>
+                                    :
+                                    myTrainings.length === 0 ?
+                                        <h5> Currently you don't have any trainings. </h5> :
+
+                                        <Carousel variant="dark" style={{height:600}}>
+                                                {
+                                                myTrainings?.map((data) => (
+                                                    <Carousel.Item >
+                                                    <Paper elevation={5} style={paperStyle1}>
+                                                        <div style={{color: "black"}}>
+
+                                                            <p> <h6> Training ID : {data.trainingId} </h6> </p>
+                                                            <p> <h6> Training Topic : {data?.training?.trainingModule?.trainingTopic} </h6> </p>
+                                                            <p> <h6> Training Description : {data?.training?.trainingModule?.trainingDesc} </h6> </p>
+                                                            <p> <h6> Training Mode : {data?.training?.trainingMode} </h6> </p>
+                                                            <p> <h6> Training Type : {data?.training?.trainingType} </h6> </p>
+                                                            <p>  <h6> Training time : {data?.training?.trainingTime} </h6> </p>
+
+
+                                                            <h4> General Details </h4>
+                                                            <h6> Is Approved : {data.isApproved} </h6>
+                                                            <h6> Approved By : {data.approvedByAgent?.client?.givenName} {data.approvedByAgent?.client?.surName} </h6>
+                                                            <h6> Approved Date : {moment(data.approvedDate).format("DD-MMM-YYYY")} </h6>
+                                                            <h6> Total Days : {data.totalDays} </h6>
+                                                            <h6> Total Days Attended : {data.daysAttended} </h6>
+                                                            <h6> Training Score : {data.trainingScore} </h6>
+                                                            <h6> Training Status : {data.trainingStatus} </h6>
+                                                            <h6> Comments : {data.comments} </h6>
+                                                        </div>
+                                                    </Paper>
+                                                    </Carousel.Item>
+                                                ))}
+                                        </Carousel>
+                            }
+                        </div>
+                    </div>
+                </div>
             <br/>
-            <div>
-                <h3 style={{marginLeft:400}}> My Trainings :- </h3>
-                {
-                    sessionStorage.getItem("agent" ) === "null"  ?
-                        <h4 style={{marginLeft:425}}> You are not a agent yet. </h4>
-                        :
-                            myTrainings.length === 0 ?
-                            <h5 style={{marginLeft:425}}> Currently you don't have any trainings. </h5> :
-                            myTrainings?.map((data) => (
-                                <>
-                                    <Paper elevation={5} style={paperStyle1}>
-                                        <div style={{color: "black"}}>
-
-                                            <p> <h6> Training ID : {data.trainingId} </h6> </p>
-                                            <p> <h6> Training Topic : {data?.training?.trainingModule?.trainingTopic} </h6> </p>
-                                            <p> <h6> Training Description : {data?.training?.trainingModule?.trainingDesc} </h6> </p>
-                                            <p> <h6> Training Mode : {data?.training?.trainingMode} </h6> </p>
-                                            <p> <h6> Training Type : {data?.training?.trainingType} </h6> </p>
-                                            <p>  <h6> Training time : {data?.training?.trainingTime} </h6> </p>
 
 
-                                            <h4> General Details </h4>
-                                            <h6> Is Approved : {data.isApproved} </h6>
-                                            <h6> Approved By : {data.approvedByAgent?.client?.givenName} {data.approvedByAgent?.client?.surName} </h6>
-                                            <h6> Approved Date : {moment(data.approvedDate).format("DD-MMM-YYYY")} </h6>
-                                            <h6> Total Days : {data.totalDays} </h6>
-                                            <h6> Total Days Attended : {data.daysAttended} </h6>
-                                            <h6> Training Score : {data.trainingScore} </h6>
-                                            <h6> Training Status : {data.trainingStatus} </h6>
-                                            <h6> Comments : {data.comments} </h6>
-                                        </div>
-                                    </Paper>
-                                </>
-                            ))
-                }
-
-
-            </div>
 
             <Modal
                 show={modal}
