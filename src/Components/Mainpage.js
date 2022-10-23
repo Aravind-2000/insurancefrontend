e344d5958fabc77b290e4cb3e25d0df28a90fe17
Sanara-1930 <lakshmi.nv@futurainstech.com>
2022-10-23 18:53:16 +0530

    removed unused props

--- src/Components/Mainpage.js
+++ src/Components/Mainpage.js
@@ -1,80 +1,60 @@
-import React, {useState} from 'react';
-import {Form, Modal} from "react-bootstrap";
+import React, { useState } from "react";
+import { Form, Modal } from "react-bootstrap";
 
+const Mainpage = () => {
+  const [termsModal, setTermsModal] = useState(false);
+  const showtermsmodal = () => setTermsModal(true);
+  const hidetermsmodal = () => {
+    setTermsModal(false);
+  };
 
+  const gotoadd = (e) => {
+    e.preventDefault();
+    window.location = "/ui/add";
+  };
 
-const Mainpage = (props) => {
+  return (
+    <div>
+      <br /> <br />
+      <div className="container">
+        <button className="btn btn-danger" onClick={showtermsmodal}>
+          Enroll Now
+        </button>
+        <br /> <br />
+      </div>
+      <Modal
+        show={termsModal}
+        onHide={hidetermsmodal}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Disclaimer</Modal.Title>
+        </Modal.Header>
 
-    const [termsModal, setTermsModal] = useState(false);
-    const showtermsmodal = () => setTermsModal(true);
-    const hidetermsmodal = () => {
-        setTermsModal(false);
-    }
+        <Modal.Body>
+          <Form className="container" onSubmit={(e) => gotoadd(e)}>
+            <Form.Group>
+              <Form.Check
+                label="By checking this you accept to our terms and conditions"
+                id="terms"
+                required
+              />
+              <Form.Check
+                label="I am ready to share my personal details with your company."
+                id="self"
+                required
+              />
 
-    const gotoadd = (e) => {
-        e.preventDefault();
-        window.location = "/ui/add";
-    }
-
-
-    return (
-        <div>
-            <br/> <br/>
-            <div className="container">
-
-                <button
-                    className="btn btn-danger"
-                    onClick={showtermsmodal}
-                    >
-                    Enroll Now
-                </button>
-<br/> <br/>
-
-            </div>
-
-            <Modal
-                show={termsModal}
-                onHide={hidetermsmodal}
-                backdrop="static"
-                keyboard={false}
-             >
-
-               <Modal.Header closeButton>
-                   <Modal.Title>
-                       Disclaimer
-                   </Modal.Title>
-               </Modal.Header>
-
-                <Modal.Body>
-                    <Form className="container" onSubmit={(e) => gotoadd(e)}>
-
-                        <Form.Group>
-
-                            <Form.Check
-                                label="By checking this you accept to our terms and conditions"
-                                id="terms"
-                                required
-                            />
-                            <Form.Check
-                                label="I am ready to share my personal details with your company."
-                                id="self"
-                                required
-                            />
-
-                            <button
-                                type="submit button"
-                                className="btn btn-danger"
-                            >
-                                Ok, I understand
-
-                            </button>
-                        </Form.Group>
-                    </Form>
-                </Modal.Body>
-            </Modal>
-
-        </div>
-    );
+              <button type="submit button" className="btn btn-danger">
+                Ok, I understand
+              </button>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
 };
 
 export default Mainpage;
